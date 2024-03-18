cf78f16c4b4a71c88998ce5db4f251dc6574c7bc
Jonas Hennig <jonass.hennig@dicebridge.com>
2024-03-18 11:36:27 +0100

    Add Database Downloader and Connector

---
+++ DDC.py
@@ -0,0 +1,18 @@
+
+
+class DatabaseDownloderConnector:
+    def __init__(self) -> None:
+        pass
+
+
+    def download_files(self):
+        pass
+
+    def delete_files(self):
+        pass
+
+    def insert_files_into_database(self):
+        pass
+
+    def start_evaluation(self):
+        pass
